d34cf749172c3466e8f4b7f1a2419b7575b8df3f
kendi ardi <arsamisapper12@gmail.com>
2018-03-23 11:04:19 +0800

    Tambahan

--- src/tugas1/senjataApi_Aksi.java
+++ src/tugas1/senjataApi_Aksi.java
@@ -12,6 +12,8 @@ package tugas1;
 public class senjataApi_Aksi {
     public static void main(String[] args) {
         senjataApi M4A1 = new senjataApi();
+        senjataApi AK47 = new senjataApi();
+        senjataApi SCARL = new senjataApi();
         
         M4A1.jarakTembak="710M";
         M4A1.jenisPeluru="Timah";
@@ -20,5 +22,21 @@ public class senjataApi_Aksi {
         M4A1.warnaSenjata="Hitam";
         
         M4A1.cetakInfo();
+    
+        AK47.jarakTembak="530M";
+        AK47.jenisPeluru="Timah";
+        AK47.beratSenjata="7 Kg";
+        AK47.kapasitasPeluru="30 s/d 100 Peluru per satu slot/magazine";
+        AK47.warnaSenjata="Coklat Tua, Hitam";
+        
+        AK47.cetakInfo();
+        
+        SCARL.jarakTembak="810M";
+        SCARL.jenisPeluru="Timah";
+        SCARL.beratSenjata="8,3 Kg";
+        SCARL.kapasitasPeluru="20 s/d 40 Peluru per satu slot/magazine";
+        SCARL.warnaSenjata="Coklat Muda";
+        
+        SCARL.cetakInfo();
     }
 }
